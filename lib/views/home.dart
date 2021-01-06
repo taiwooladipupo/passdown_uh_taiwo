55327b08f52e3509640adac1dc705b8199ed2f8b
Telleosis <telleosis@gmail.com>
2021-01-06 22:32:56 +0100

    More Changes Committed... 1. Refactored the Scrollable Grid List added to home screen to use GridBuilder class 2. Implemented a persistent footer with a signout button

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -3,42 +3,50 @@ import 'package:passdown/authentication_service.dart';
 import 'package:passdown/theme/routes.dart';
 import 'package:provider/provider.dart';
 
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
+class HomePage extends StatelessWidget {
+  final List<Map> myProducts =
+      List.generate(6, (index) => {"id": index, "name": "Product $index"})
+          .toList();
 
-class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    final title = 'Grid List';
-
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.deepPurple.shade900,
-        title: Text('Welcome Home'),
+        title: Text('PASSDOWN'),
       ),
-      body: GridView.count(
-        // Create a grid with 2 columns. If you change the scrollDirection to
-        // horizontal, this produces 2 rows.
-          crossAxisCount: 2,
-          // Generate 100 widgets that display their index in the List.
-          children: List.generate(10, (index) {
-            return Center(
-              child: Text(
-                'Item $index',
-                style: Theme.of(context).textTheme.headline5,
-              ),
-             child: RaisedButton(
-                    onPressed: () {
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: GridView.builder(
+            gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                maxCrossAxisExtent: 200,
+                childAspectRatio: 3 / 2,
+                crossAxisSpacing: 20,
+                mainAxisSpacing: 20),
+            itemCount: myProducts.length,
+            itemBuilder: (BuildContext context, index) {
+              return Container(
+                alignment: Alignment.center,
+                child: Text(myProducts[index]["name"]),
+                decoration: BoxDecoration(
+                  color: Colors.amber,
+                  borderRadius: BorderRadius.circular(15),
+                ),
+              );
+            }),
+      ),
+      persistentFooterButtons: [
+          Padding(
+           padding: const EdgeInsets.only(right: 150.0),
+           child: RaisedButton(
+            onPressed: () {
               context.read<AuthenticationService>().SignOut();
               Navigator.of(context).pushNamed(AppRoutes.authLogin);
-              },
-                child: Text('Sign Out'),
-              ),
-            );
-          }),
-      ),
+            },
+            child: Text('Sign Out'),
+          ),
+        ),
+      ],
     );
   }
 }
